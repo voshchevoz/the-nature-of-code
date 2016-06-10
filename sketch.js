2c0272d3fc6bc7b6236a1457e3a00d59ba1a28a1
Andrew Voshchevoz <voshchevoz@gmail.com>
2016-06-10 17:34:15 +0300

    FInalized SAW implementation

--- sketch.js
+++ sketch.js
@@ -1,76 +1,103 @@
-var w;
-var img;
+var w = new Array(0);
 
 function setup() {
   createCanvas(640, 360);
-  img = loadImage("poop.png");
-  
+  frameRate(5);
+
   // Make a Walker object
-  w = new Walker();
+  var c = color('rgba(100%,0%,0%,0.5)');
+  append(w, new Walker(10, c, width / 2, height / 2))
 }
 
 function draw() {
   background(51);
   // Update and display object
-  w.update();
-  w.display();
+
+  for (i = 0; i < w.length; i++) {
+    w[i].update();
+    w[i].display();
+  }
 }
 
-function Walker() {
-  // Start Walker in center
-  this.posCache = [createVector(width / 2, height / 2)];
+// create random walker at (x, y) coordinate with given stroke width and given color
+function Walker(aStroke, aColor, x, y) {
+  // Start Walker in center and initialize cache of coordinates visited by the walker
+  this.posCache = [createVector(x, y)];
 
-  // Start Walker in center
-  
-  //this.vel = createVector(0, 0);
-  
+  // stroke width
+  this.stroke = aStroke;
+  // stroke color
+  this.color = aColor;
+
+  // if true SAW can't make any further step
+  this.deadend = false;
+
+
+  // this function checks if a walker has already visited coordinate newCoord
   this.isValidStep = function(newCoord) {
     for (j = this.posCache.length - 1; j >= 0; j--) {
       if (this.posCache[j].x == newCoord.x && this.posCache[j].y == newCoord.y) {
         return false;
       }
-      return true;
     }
+    return true;
+  }
+
+  // given last step this function generated an array of valid steps from this point
+  this.getValidSteps = function(lastStep) {
+    var res = new Array(0);
+    var step = p5.Vector.add(lastStep, createVector(this.stroke, 0));
+    if (this.isValidStep(step)) {
+      append(res, createVector(step.x, step.y));
+    }
+    step = p5.Vector.add(lastStep, createVector(-this.stroke, 0));
+    if (this.isValidStep(step)) {
+      append(res, createVector(step.x, step.y));
+    }
+    step = p5.Vector.add(lastStep, createVector(0, this.stroke));
+    if (this.isValidStep(step)) {
+      append(res, createVector(step.x, step.y));
+    }
+    step = p5.Vector.add(lastStep, createVector(0, -this.stroke));
+    if (this.isValidStep(step)) {
+      append(res, createVector(step.x, step.y));
+    }
+
+    return res;
   }
 
   this.update = function() {
+    // init new coordinate with the last walker step. copy to a new vector
     var lastStep = this.posCache[this.posCache.length - 1];
-    var newPos = createVector(lastStep.x, lastStep.y);
+    //var newPos = createVector(lastStep.x, lastStep.y);
 
-    while (!this.isValidStep(newPos)) {
-      var motion = floor(random(4));
-      switch(motion) {
-        case 0:
-          newPos.add(createVector(1, 0));
-          break;
-        case 1:
-          newPos.add(createVector(-1, 0));
-          break;
-        case 2:
-          newPos.add(createVector(0, 1));
-          break;
-        case 3:
-          newPos.add(createVector(0, -1));
-          break;
+    // randomly generate new step from a set of valid steps
+    // if no valid steps exists mark this path as deadend
+    if (!this.deadend) {
+      var validSteps = this.getValidSteps(lastStep);
+      var len = validSteps.length;
+      if (len != 0) {
+        var motion = floor(random(len));
+        var newPos = validSteps[motion];
+        // add new step to the cache of visited coordinates
+        append(this.posCache, newPos);
+      }
+      else {
+        this.deadend = true;
       }
     }
-
-  //alert("updated")
-    append(this.posCache, newPos);
   }
 
   this.display = function() {
-    // Draw Walker as circle
-    //fill(255);
-    //rotate(PI*random(-1, 1));
-    //image(img, this.pos.x, this.pos.y);
-    
-    strokeWeight(1);
-    stroke("red");
+    strokeWeight(this.stroke);
+    stroke(this.color);
     for (i = 0; i < this.posCache.length; i++) {
       point(this.posCache[i].x, this.posCache[i].y);
     }
-    
-    // ellipse(this.pos.x, this.pos.y, 48, 48);
+
+    if (this.deadend) {
+      text("dead-end", 10, 30);
+    }
+
   }
 }
