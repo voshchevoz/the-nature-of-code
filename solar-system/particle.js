0d6520a07d9a89e61b7391d8299bec9f658723a7
Andrew Voshchevoz <voshchevoz@gmail.com>
2016-06-15 17:34:56 +0300

    Added possibility to drag particles and to change velocity vector direction. Simulation now can be started/stopped

--- solar-system/particle.js
+++ solar-system/particle.js
@@ -1,16 +1,36 @@
-var Particle = function() {
-  this.pos = createVector(400, 50);
+var Particle = function(x, y, m) {
+  this.pos = createVector(x, y);
   this.vel = createVector(1, 0);
   this.acc = createVector(0, 0);
-  this.mass = 1;
+  this.mass = m;
   this.radius = this.mass * 16;
+  this.selected = false;
+  // this stores velocity vector unnormalized - required to render vector itself
+  this.velCoord = createVector(0, 0);
+
+  this.select = function() {
+    // mark as selected if mouse is inside the circle
+    if(dist(mouseX, mouseY, this.pos.x, this.pos.y) < this.radius) {
+      this.selected = true;
+    }
+    // otherwise deselect
+    else {
+      this.selected = false;
+    }
+  }
+
+  this.update = function() {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+  }
 
   this.display = function() {
     stroke(0);
     strokeWeight(2);
     //fill(255, 127);
 
-    if(dist(mouseX, mouseY, this.pos.x, this.pos.y) < this.radius) {
+    if(this.selected) {
       //mouse is inside circle
 
       if(mouseIsPressed) {
@@ -28,11 +48,24 @@ var Particle = function() {
     }
     else{
       //mouse is outside circle
-      fill(0, 0, 255);
+      fill(0, 0, 125);
     }
 
     ellipse(this.pos.x, this.pos.y, this.radius, this.radius);
 
-    line(this.pos.x, this.pos.y, mouseX, mouseY);
+    // modify velocity line only for selected circle
+    if (this.selected) {
+      this.velCoord = createVector(mouseX, mouseY).sub(this.pos);
+
+      // normalize velocity
+      this.vel = this.velCoord.copy().normalize();
+    }
+
+    // draw velocity vector starting from circle center ending at velCoords
+    // draw only if simulation not running
+    if(!running) {
+      var lineEnd = p5.Vector.add(this.pos, this.velCoord)
+      line(this.pos.x, this.pos.y, lineEnd.x, lineEnd.y);
+    }
   }
 }
--- solar-system/sketch.js
+++ solar-system/sketch.js
@@ -1,16 +1,34 @@
-var particle;
+var particles = [];
+var running = false;
 //var attractor;
 
 function setup() {
   createCanvas(640, 360);
-  particle = new Particle(400, 50, 1);
+  particle1 = new Particle(400, 50, 1);
+  particle2 = new Particle(300, 100, 2);
+  particles.push(particle1);
+  particles.push(particle2);
   //attractor = new Attractor(width/2, height/2);
   //noLoop();
 }
 
+// adds global key listener
+function keyPressed() {
+  // starts/stops simulation by pressing space key, 32 = space key
+  if (keyCode == 32) {
+    running = !running;
+  }
+  return false;
+}
 
-function mouseDragged() {
-  console.log('drag');
+// adds global mouse listener
+function mousePressed() {
+  // allowing particles selection only if simulation is not running
+  if (!running) {
+    for (var i = 0; i < particles.length; i++) {
+      particles[i].select();
+    }
+  }
 }
 
 function draw() {
@@ -20,9 +38,19 @@ function draw() {
   // Attractor attracts particle
   //var force = attractor.calculateAttraction(particle);
   //particle.applyForce(force);
+  //
+  for (var i = 0; i < particles.length; i++) {
+    // update particles only if simulation is running
+    if (running) {
+      particles[i].update();
+    }
+    particles[i].display();
+  }
+
 
   //particle.update();
-  particle.display();
+  //particle1.display();
+  //particle2.display();
 
   //attractor.display();
 }
